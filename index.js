38422c2383329d27f7fa7caab3c8c73bbe0f6df0
william tsanga <williamtsanga@yahoo.com>
2022-01-04 10:59:02 +0100

    configure express and set up connection

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-import {sendSMS} from './api/index'
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const PORT = 5000;
+const {connectToServer,register} = require('./db/index')
+const app = express();
 
 
-
-sendSMS();
+app.use(cookieParser());
+app.get('/', async (req, res) => {
+    // try {
+    //     const user = await getUsers();
+    //     res.send(user)
+    //     console.log(`Updated documents`);
+    //   } catch (err) {
+    //     console.error(`Something went wrong:`);
+    //     res.send("error")
+    //   }  
+})
+  
+connectToServer(function (err) {
+    if (err) {
+      console.error(err);
+      process.exit();
+    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+});
