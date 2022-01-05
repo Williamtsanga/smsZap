53916cd64f275dcc8501dfd53c1f59dafd6bccc2
kevin12-del <82839973+kevin12-del@users.noreply.github.com>
2022-01-05 14:28:42 +0100

    b

---
+++ api/index.ts
@@ -0,0 +1,20 @@
+export const sendSMS = () => {
+    const apilogin="f16187da70b6";
+    const apikey="b6901d40-f211-930a-8e0c-1cdad7a64f99";
+   var  requestOptions = {
+        method: 'POST',
+        headers: { 
+            Authorization:'Basic '+ btoa(apilogin + ":" + apikey),
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+       },
+     body: JSON.stringify({ phoneNumber: '698228738',message:'123456789' })
+    };
+    fetch('http://proxysms.mufoca.com/api/v0/shortMessages', requestOptions)
+    .then(response => response.json())
+    .then(data => {console.log(data)
+       
+    })
+    .catch((err) => {
+      console.log(err)
+    })}
